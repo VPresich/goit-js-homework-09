3ae3c0f23fabfd113c24a1a8334159e4ca52bd59
VPresich <111353667+VPresich@users.noreply.github.com>
2023-11-18 15:40:18 +0000

    Deploying to gh-pages from @ VPresich/goit-js-homework-09@6edb4c23fdc034034a8c8437c6cafbf7f91b3bdf 🚀

--- commonHelpers5.js
+++ commonHelpers5.js
@@ -1,2 +1,24 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{S as e}from"./assets/vendor-c279a850.js";import{c as o,i as t}from"./assets/gallery-images-d2b92588.js";const a=document.querySelector(".gallery");o(t,a);new e(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionsDelay:250,captionType:"attr",showCounter:!0,disableScroll:!1,disableZoom:!1,history:!0,swipeClose:!0});
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{S as r}from"./assets/vendor-c279a850.js";import{i}from"./assets/icons-aa1fc6ea.js";import{i as a}from"./assets/gallery-images-8dc32416.js";const s=({preview:e,description:t,original:o})=>`<li class="gallery-item">
+              <a class="gallery-link" href="${o}">
+                <img class="gallery-img"
+                    src="${e}"
+                    alt="${t}"                    
+                 />
+              </a>
+            </li>`;function l(e,t){const o=e.map(s).join("");t.insertAdjacentHTML("beforeend",o)}const n=document.querySelector(".gallery");l(a,n);new r(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionsDelay:250,nav:!0,navText:[`<svg
+    class="lightbox-button-icon-prev"
+    width="24"
+    height="24"
+    aria-label="icon arrow left">
+      <use href="${i}#icon-chevron-left"></use>
+  </svg>`,`<svg
+    class="lightbox-button-icon-next"
+    width="24"
+    height="24"
+    aria-label="icon arrow right">
+    <use href="${i}#icon-chevron-right"></use>
+  </svg>`],close:!0,closeText:`
+  <svg class="lightbox-close-icon" width="16" height="16">
+    <use href="${i}#close-form"></use>
+  </svg>`,enableKeyboard:!0,showCounter:!0});
 //# sourceMappingURL=commonHelpers5.js.map
