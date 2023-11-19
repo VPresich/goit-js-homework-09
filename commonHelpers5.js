f7e4a3193824bf5c57c21b34858b4183164c46ae
VPresich <111353667+VPresich@users.noreply.github.com>
2023-11-19 12:43:47 +0000

    Deploying to gh-pages from @ VPresich/goit-js-homework-09@718eaf826939925876894685a16a577c6e87f6fa 🚀

--- commonHelpers5.js
+++ commonHelpers5.js
@@ -1,11 +1,11 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{S as r}from"./assets/vendor-c279a850.js";import{i}from"./assets/icons-aa1fc6ea.js";import{i as a}from"./assets/gallery-images-8dc32416.js";const s=({preview:e,description:t,original:o})=>`<li class="gallery-item">
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{S as r}from"./assets/vendor-c279a850.js";import{i}from"./assets/icons-aa1fc6ea.js";import{i as a}from"./assets/gallery-images-8dc32416.js";const l=({preview:e,description:t,original:o})=>`<li class="gallery-item">
               <a class="gallery-link" href="${o}">
                 <img class="gallery-img"
                     src="${e}"
                     alt="${t}"                    
                  />
               </a>
-            </li>`;function l(e,t){const o=e.map(s).join("");t.insertAdjacentHTML("beforeend",o)}const n=document.querySelector(".gallery");l(a,n);new r(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionsDelay:250,nav:!0,navText:[`<svg
+            </li>`;function s(e,t){const o=e.map(l).join("");t.insertAdjacentHTML("beforeend",o)}const n=document.querySelector(".gallery");s(a,n);new r(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionsDelay:250,nav:!0,navText:[`<svg
     class="lightbox-button-icon-prev"
     width="24"
     height="24"
@@ -20,5 +20,5 @@ import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                
   </svg>`],close:!0,closeText:`
   <svg class="lightbox-close-icon" width="16" height="16">
     <use href="${i}#close-form"></use>
-  </svg>`,enableKeyboard:!0,showCounter:!0});
+  </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0});
 //# sourceMappingURL=commonHelpers5.js.map
