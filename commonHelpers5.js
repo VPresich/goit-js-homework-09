691931a8fb39703be0e16090b180429f58fde7d3
VPresich <111353667+VPresich@users.noreply.github.com>
2023-11-20 15:37:45 +0000

    Deploying to gh-pages from @ VPresich/goit-js-homework-09@a42fe73b80ac3ee21379e50a8e2f08d6145aedc5 🚀

--- commonHelpers5.js
+++ commonHelpers5.js
@@ -1,24 +1,24 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{S as r}from"./assets/vendor-c279a850.js";import{i}from"./assets/icons-aa1fc6ea.js";import{i as a}from"./assets/gallery-images-8dc32416.js";const l=({preview:e,description:t,original:o})=>`<li class="gallery-item">
-              <a class="gallery-link" href="${o}">
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{S as a}from"./assets/vendor-c279a850.js";import{i as o}from"./assets/icons-aa1fc6ea.js";import{i as r}from"./assets/gallery-images-8dc32416.js";const l=({preview:e,description:t,original:i})=>`<li class="gallery-item">
+              <a class="gallery-link" href="${i}">
                 <img class="gallery-img"
                     src="${e}"
                     alt="${t}"                    
                  />
               </a>
-            </li>`;function s(e,t){const o=e.map(l).join("");t.insertAdjacentHTML("beforeend",o)}const n=document.querySelector(".gallery");s(a,n);new r(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionsDelay:250,nav:!0,navText:[`<svg
+            </li>`;function s(e,t){const i=e.map(l).join("");t.insertAdjacentHTML("beforeend",i)}const n=document.querySelector(".gallery");s(r,n);new a(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionsDelay:250,nav:!0,navText:[`<svg
     class="lightbox-button-icon-prev"
     width="24"
     height="24"
     aria-label="icon arrow left">
-      <use href="${i}#icon-chevron-left"></use>
+      <use href="${o}#icon-chevron-left"></use>
   </svg>`,`<svg
     class="lightbox-button-icon-next"
     width="24"
     height="24"
     aria-label="icon arrow right">
-    <use href="${i}#icon-chevron-right"></use>
+    <use href="${o}#icon-chevron-right"></use>
   </svg>`],close:!0,closeText:`
   <svg class="lightbox-close-icon" width="16" height="16">
-    <use href="${i}#close-form"></use>
-  </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0});
+    <use href="${o}#close-form"></use>
+  </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0,widthRatio:.85,heightRatio:.9});
 //# sourceMappingURL=commonHelpers5.js.map
